09f5769c28deef7f5af3806d27310e322325b8c3
ajaymathpal <167537051+ajaymathpal@users.noreply.github.com>
2025-11-13 19:47:16 +0530

    Add HCF calculation program in Ques4.java
    Implement HCF calculation using subtraction method

---
+++ Ques4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class HCF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        while(a != b) {
+            if(a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println(a);
+    }
+}
