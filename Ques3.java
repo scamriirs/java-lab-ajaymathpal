7ba777659caf74dd98989bca77996ca4f38c9e9c
ajaymathpal <167537051+ajaymathpal@users.noreply.github.com>
2025-11-13 19:46:54 +0530

    Create Ques3.java

---
+++ Ques3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), r, s = 0, t = n;
+        while(n > 0) {
+            r = n % 10;
+            s = s * 10 + r;
+            n /= 10;
+        }
+        if(t == s)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
