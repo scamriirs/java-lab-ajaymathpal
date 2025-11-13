c776fafa574262c90f0fb092fc11965a8860c855
ajaymathpal <167537051+ajaymathpal@users.noreply.github.com>
2025-11-13 19:55:14 +0530

    Implement MyThread class with runnable example

---
+++ Ques20.java
@@ -0,0 +1,9 @@
+class MyThread implements Runnable {
+    public void run() {
+        for(int i=1;i<=5;i++) System.out.println(Thread.currentThread().getName()+" "+i);
+    }
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyThread(), "Thread1");
+        t1.start();
+    }
+}
