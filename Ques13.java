cd85280d9e99c18aa66441a5ad214fafc62034b1
ajaymathpal <167537051+ajaymathpal@users.noreply.github.com>
2025-11-13 19:50:44 +0530

    Implement Vehicle and Car classes with display method

---
+++ Ques13.java
@@ -0,0 +1,26 @@
+class Vehicle {
+    String vehicle_type;
+    Vehicle(String v) {
+        vehicle_type = v;
+    }
+    void display() {
+        System.out.println("Vehicle type: " + vehicle_type);
+    }
+}
+class Car extends Vehicle {
+    String model_type, company;
+    Car(String v, String m, String c) {
+        super(v);
+        model_type = m;
+        company = c;
+    }
+    void display() {
+        super.display();
+        System.out.println("Model: " + model_type);
+        System.out.println("Company: " + company);
+    }
+    public static void main(String[] args) {
+        Car c = new Car("Four Wheeler", "Sedan", "Honda");
+        c.display();
+    }
+}
