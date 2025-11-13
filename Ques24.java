8828af45550cdf7fba18a5d804d50bc194c1e03a
ajaymathpal <167537051+ajaymathpal@users.noreply.github.com>
2025-11-13 19:56:41 +0530

    Implement age validation with custom exception

---
+++ Ques24.java
@@ -0,0 +1,18 @@
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String s) {
+        super(s);
+    }
+}
+class Test {
+    static void check(int age) throws InvalidAgeException {
+        if(age < 18) throw new InvalidAgeException("Not eligible");
+        else System.out.println("Eligible");
+    }
+    public static void main(String[] args) {
+        try {
+            check(15);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
