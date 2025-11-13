b499ec6c743bcbfc8bd7bc364b90ab685850343c
ajaymathpal <167537051+ajaymathpal@users.noreply.github.com>
2025-11-13 19:50:02 +0530

    Create Ques11.java

---
+++ Ques11.java
@@ -0,0 +1,21 @@
+class Student {
+    String name;
+    int age;
+    Student() {
+        name = "Ajay";
+        age = 20;
+    }
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+    void display() {
+        System.out.println(name + " " + age);
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Rahul", 22);
+        s1.display();
+        s2.display();
+    }
+}
