3c55cae2998b9ca5ef3274e5d0c9e1d10935b56b
ajaymathpal <167537051+ajaymathpal@users.noreply.github.com>
2025-11-13 19:47:37 +0530

    Add SeriesSum class to calculate harmonic series sum

---
+++ Ques5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SeriesSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sum = 0;
+        for(int i=1; i<=n; i++)
+            sum += 1.0/i;
+        System.out.println(sum);
+    }
+}
